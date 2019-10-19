4534b2c4f44b4e4ec57e88d7e7d30046c5d0acf4
bygagaup <balakin.2000@yandex.ru>
2019-10-20 02:29:37 +0300

    Новая карта 5x5

--- src/map_creation.c
+++ src/map_creation.c
@@ -19,18 +19,18 @@ t_point		***map_creation(t_point *po, t_val *val)
 	int		y;
 
 	y = 0;
-	map = (t_point***)malloc(sizeof(t_point**) * (val->max_y + 1));
+	map = (t_point***)malloc(sizeof(t_point**) * (val->max_y + 2));
 	while (po != NULL)
 	{
 		x = 0;
-		map[y] = (t_point**)malloc(sizeof(t_point*) * (val->max_x + 1));
+		map[y] = (t_point**)malloc(sizeof(t_point*) * (val->max_x + 2));
 		while (y == po->y)
 		{
 			map[y][x] = po;
 			po = po->next;
 			x++;
 		}
-		map[y][x + 1] = NULL;
+		map[y][x] = NULL;
 		y++;
 	}
 	map[y] = NULL;
