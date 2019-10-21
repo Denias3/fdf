050b7d5a74053e4dbfecf50d34d5b6a8e2c743da
bygagaup <balakin.2000@yandex.ru>
2019-10-21 21:35:02 +0300

    Испрвил gnl

--- src/init_window.c
+++ src/init_window.c
@@ -13,22 +13,77 @@
 #include "../include/fdf.h"
 #include <stdio.h>
 
+void	drawLine(int x1, int y1, int x2, int y2) {
+	const int deltaX = abs(x2 - x1);
+	const int deltaY = abs(y2 - y1);
+	const int signX = x1 < x2 ? 1 : -1;
+	const int signY = y1 < y2 ? 1 : -1;
+	int error = deltaX - deltaY;
+	int error2;
+
+	mlx_pixel_put(paint->mlx_ptr, paint->win_ptr, x2, y2, 0xff0505);
+	while(x1 != x2 || y1 != y2)
+	{
+		mlx_pixel_put(paint->mlx_ptr, paint->win_ptr, x1, y1, 0xff0505);
+		error2 = error * 2;
+		if(error2 > -deltaY)
+		{
+			error -= deltaY;
+			x1 += signX;
+		}
+		if(error2 < deltaX)
+		{
+			error += deltaX;
+			y1 += signY;
+		}
+	}
+}
+
 int	deal_key(int key, void *param)
 {
 	t_paint *paint;
 
 	paint = (t_paint*)param;
-	if (key == 126)
-		(paint->y)--;
-	else if (key == 125)
-		(paint->y)++;
-	else if (key == 124)
-		(paint->x)++;
-	else if (key == 123)
-		(paint->x)--;
 
-	printf("x - %d\t\ty - %d\n", paint->x, paint->y);
-	mlx_pixel_put(paint->mlx_ptr, paint->win_ptr, paint->x, paint->y, 0xff0505);
+
+	int 	x1;
+	int 	x2;
+	int 	y1;
+	int 	y2;
+
+	if (key == 126)
+	{
+		x1 = 10;
+		x2 = 30;
+		y1 = 30;
+		y2 = 70;
+		const int deltaX = abs(x2 - x1);
+		const int deltaY = abs(y2 - y1);
+		const int signX = x1 < x2 ? 1 : -1;
+		const int signY = y1 < y2 ? 1 : -1;
+		//
+		int error = deltaX - deltaY;
+		//
+		mlx_pixel_put(paint->mlx_ptr, paint->win_ptr, x2, y2, 0xff0505);
+		while(x1 != x2 || y1 != y2)
+		{
+			mlx_pixel_put(paint->mlx_ptr, paint->win_ptr, x1, y1, 0xff0505);
+			const int error2 = error * 2;
+			//
+			if(error2 > -deltaY)
+			{
+				error -= deltaY;
+				x1 += signX;
+			}
+			if(error2 < deltaX)
+			{
+				error += deltaX;
+				y1 += signY;
+			}
+		}
+	}
+
+
 	return (0);
 }
 
